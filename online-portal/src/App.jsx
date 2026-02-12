b0d90747595aa23c06ddc9f4725f2102623adf43
saubhagya02-bit <saubhagyaushani03@gmail.com>
2026-02-12 11:52:18 +0530

    Update

--- online-portal/src/App.jsx
+++ online-portal/src/App.jsx
@@ -4,21 +4,23 @@ import RegisterPage from "./pages/RegisterPage";
 import HomePage from "./pages/HomePage";
 import ReservationPage from "./pages/ReservationPage";
 import NavBar from "./components/NavBar";
-import StallMap from "./pages/StallMap";
+import { AuthProvider } from "./contexts/AuthContext";
+import AboutPage from "./pages/AboutPage";
 
 function App() {
   return (
-    <>
+    <AuthProvider>
       <NavBar />
 
       <Routes>
         <Route path="/" element={<LoginPage />} />
         <Route path="/register" element={<RegisterPage />} />
+        <Route path="/about" element={<AboutPage />} />
         <Route path="/home" element={<HomePage />} />
-        <Route path="/reserve" element={<ReservationPage />} />
-        <Route path="/stallmap" element={<StallMap />} />
+        <Route path="/stalls" element={<ReservationPage />} />
+        <Route path="/my-reservations" element={<div>My Reservations Page</div>} />
       </Routes>
-    </>
+    </AuthProvider>
   );
 }
 
