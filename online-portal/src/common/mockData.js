fe34bab41b87f0574ba5e5581e526ecb639bfd9a
Pabodha Wanniarachchi <pabodhawanniarachchi@gmail.com>
2026-02-12 22:03:13 +0530

    Add common source files

---
+++ online-portal/src/common/mockData.js
@@ -0,0 +1,175 @@
+export const mockStalls = [
+  // --- Large Stalls (4gridRow4) ---
+  {
+    id: "A1",
+    size: "Large",
+    gridRow: 1,
+    gridCol: 1,
+    isConfirmed: 0,
+    price: 25000,
+  },
+  {
+    id: "A2",
+    size: "Large",
+    gridRow: 2,
+    gridCol: 1,
+    isConfirmed: 0,
+    price: 25000,
+  },
+  {
+    id: "A3",
+    size: "Large",
+    gridRow: 3,
+    gridCol: 1,
+    isConfirmed: 0,
+    price: 25000,
+  },
+
+  // --- Medium Stalls (2gridRow2) ---
+  {
+    id: "B1",
+    size: "Medium",
+    gridRow: 1,
+    gridCol: 2,
+    isConfirmed: 0,
+    price: 15000,
+  },
+  {
+    id: "B2",
+    size: "Medium",
+    gridRow: 2,
+    gridCol: 2,
+    isConfirmed: 1,
+    price: 15000,
+  },
+  {
+    id: "B3",
+    size: "Medium",
+    gridRow: 3,
+    gridCol: 2,
+    isConfirmed: 0,
+    price: 15000,
+  },
+  {
+    id: "B6",
+    size: "Medium",
+    gridRow: 6,
+    gridCol: 2,
+    isConfirmed: 1,
+    price: 15000,
+  },
+
+  // --- Small Stalls (1gridRow1) ---
+  {
+    id: "C1",
+    size: "Small",
+    gridRow: 1,
+    gridCol: 3,
+    isConfirmed: 0,
+    price: 5000,
+  },
+  {
+    id: "C5",
+    size: "Small",
+    gridRow: 5,
+    gridCol: 3,
+    isConfirmed: 0,
+    price: 5000,
+  },
+  {
+    id: "C6",
+    size: "Small",
+    gridRow: 6,
+    gridCol: 3,
+    isConfirmed: 1,
+    price: 5000,
+  },
+];
+
+export const mockStalls2 = [
+  // --- Large Stalls (4gridRow4) ---
+  {
+    id: "L1",
+    size: "Large",
+    gridRow: 2,
+    gridCol: 2,
+    isConfirmed: 0,
+    price: 25000,
+  },
+  {
+    id: "L2",
+    size: "Large",
+    gridRow: 2,
+    gridCol: 14,
+    isConfirmed: 0,
+    price: 25000,
+  },
+  {
+    id: "L3",
+    size: "Large",
+    gridRow: 8,
+    gridCol: 8,
+    isConfirmed: 0,
+    price: 25000,
+  },
+
+  // --- Medium Stalls (2gridRow2) ---
+  {
+    id: "M1",
+    size: "Medium",
+    gridRow: 2,
+    gridCol: 7,
+    isConfirmed: 0,
+    price: 15000,
+  },
+  {
+    id: "M2",
+    size: "Medium",
+    gridRow: 7,
+    gridCol: 2,
+    isConfirmed: 0,
+    price: 15000,
+  },
+  {
+    id: "M3",
+    size: "Medium",
+    gridRow: 7,
+    gridCol: 5,
+    isConfirmed: 0,
+    price: 15000,
+  },
+  {
+    id: "M6",
+    size: "Medium",
+    gridRow: 7,
+    gridCol: 13,
+    isConfirmed: 0,
+    price: 15000,
+  },
+
+  // --- Small Stalls (1gridRow1) ---
+  {
+    id: "S1",
+    size: "Small",
+    gridRow: 3,
+    gridCol: 12,
+    isConfirmed: 0,
+    price: 5000,
+  },
+  {
+    id: "S5",
+    size: "Small",
+    gridRow: 11,
+    gridCol: 2,
+    isConfirmed: 0,
+    price: 5000,
+  },
+  {
+    id: "S6",
+    size: "Small",
+    gridRow: 9,
+    gridCol: 2,
+    isConfirmed: 0,
+    price: 5000,
+  },
+];
